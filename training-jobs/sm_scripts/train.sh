83bde8bc26020744f866a5ef5e05a5755e131c8f
zhangchg <zhangchg@amazon.com>
2024-05-12 04:41:35 +0000

    add uploading to s3 per epoch, inference using raw size

--- training-jobs/sm_scripts/train.sh
+++ training-jobs/sm_scripts/train.sh
@@ -10,16 +10,17 @@ pip install -e skillful_nowcasting
 
 python -u run.py --num_input_frames 4 --num_forecast_frames 18 \
                     --pretrained_model_path models/dgmr \
-                    --train_data_dir data/zuimei-radar-cropped-debug/train \
-                    --valid_data_dir data/zuimei-radar-cropped-debug/valid \
-                    --output_dir checkpoint/dgmr_forecast20_ep2 \
+                    --train_data_dir data/zuimei-radar-cropped/train \
+                    --valid_data_dir data/zuimei-radar-cropped/valid \
+                    --output_dir checkpoint/dgmr_forecast18_ep50 \
                     --num_train_epochs 2 --train_batch_size 1 --valid_batch_size 1\
                     --mixed_precision 16-mixed \
                     --accelerator_device gpu \
-                    --num_devices 4 \
+                    --num_devices 8 \
                     --strategy ddp \
                     --dataloader_num_workers 4 \
-                    --validation_steps 10 \
-                    --checkpointing_steps 20 \
-                    --checkpoints_total_limit 5
+                    --validation_steps 5 \
+                    --checkpointing_steps 10 \
+                    --checkpoints_total_limit 10 \
+                    --max_nonzero_ratio 0.5
         
